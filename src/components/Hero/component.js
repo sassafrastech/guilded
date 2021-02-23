375685e524f84dd3dbf6cf72d59b4e2d026152cf
Kevin Cooper <cooperka92@gmail.com>
2021-02-22 22:05:39 -0800

    Add rasterized placeholder hero image

--- src/components/Hero/component.js
+++ src/components/Hero/component.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core';
 
 import logo from '../../assets/logo.svg';
+import hero from '../../assets/hero.jpg';
 
 const useStyles = makeStyles({
   container: {},
@@ -12,8 +13,15 @@ const Hero = () => {
 
   return (
     <div className={classes.container}>
+      <img src={hero} alt="Woman brainstorming on whiteboard" />
       <img src={logo} alt="Guilded Logo" />
-      <div>Hello world</div>
+      <div>True empowerment for freelancers</div>
+      <div>
+        Guilded is a freelancer worker cooperative that focuses on the agency,
+        decision-making and economic power of the freelancer. We offer
+        invoicing, tax preparation, health care and advocacy support so that you
+        can focus on doing what you do best. Scroll down to learn more.
+      </div>
     </div>
   );
 };
